c4a5c9e4a6875646c66dcf59d9e0cd00c5f3aa90
mehtaajay591 <mehtaajay591@gmail.com>
2020-07-05 20:31:30 +0530

    api updated and json added

--- src/app/admin/services/analysis-api.service.ts
+++ src/app/admin/services/analysis-api.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FilterModel } from '../models/Filters.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalysisApiService {
 
+  readonly FETCH_ANALYSIS_DOMAINS = environment.apiUrl + '/admin/AnalysisResultTable.json';
+
   constructor(private http: HttpClient) { }
 
   getAnalysisResult(filters: FilterModel) {
-    return this.http.get("../../../assets/JSON/AnalysisResultTable.json");//;,filters);
+    return this.http.get(this.FETCH_ANALYSIS_DOMAINS);//;,filters);
   }
 }
