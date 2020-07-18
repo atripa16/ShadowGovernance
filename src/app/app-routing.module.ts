40c3cb30641b3ca2a329283b6400494697becd05
Abhishek <abhishektripathi.10000@gmail.com>
2020-07-18 11:03:46 +0530

    unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddResourceDetailsComponent } from './end-user/components/add-resource-details/add-resource-details.component';
 
 const routes: Routes = [
   {
