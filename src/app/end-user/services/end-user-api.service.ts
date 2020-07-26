9dc9ec91ba23541ac0f599c24569d3041975e9fb
Abhishek <abhishektripathi.10000@gmail.com>
2020-07-26 11:13:28 +0530

    added params

--- src/app/end-user/services/end-user-api.service.ts
+++ src/app/end-user/services/end-user-api.service.ts
@@ -3,7 +3,7 @@ import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { EmployeeInfo } from '../models/employee-data.model';
 import { TaskDescription } from '../models/task-description.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +22,15 @@ export class EndUserApiService {
 
 
   fetchEndUserDetails(id: number, dates: string[]): Observable<EmployeeInfo> {
-    return this.http.get<EmployeeInfo>(this.FETCH_END_USER_DOMAINS);
+    const httpParams = new HttpParams().append('id', id.toString())
+      .append('dates', dates.toString());
+    return this.http.get<EmployeeInfo>(this.FETCH_END_USER_DOMAINS, { params: httpParams });
   }
 
   loadCurrWeekTasks(id: number, dates: string[]): Observable<TaskDescription[]> {
-    return this.http.get<TaskDescription[]>(this.LOAD_CURR_WEEK_TASKS);
+    const httpParams = new HttpParams().append('id', id.toString())
+      .append('dates', dates.toString());
+    return this.http.get<TaskDescription[]>(this.LOAD_CURR_WEEK_TASKS, { params: httpParams });
   }
 
   insertEndUserDetails(employeeInfo: EmployeeInfo): Observable<void> {
